56657c179e23e779428ef13852f6b945491e1287
shuting_chang <37363421+shuuuuting@users.noreply.github.com>
2020-07-19 13:20:31 +0800

    Add files via upload

---
+++ code/main.cpp
@@ -0,0 +1,23 @@
+/*==========================================================
+ This program is used to demonstrate 
+  1. how to use data structure for composite data 
+  2. how to use dynamic memory allocation
+  Created by Jiann-Jone Chen, 2013.09.25
+  =========================================================
+ */
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "student.h"
+using namespace std;
+void process1();
+
+int main() {
+	//student s1[100]; /* Static memory allocation */
+	process1();
+	system("pause");
+}
+
+
+
